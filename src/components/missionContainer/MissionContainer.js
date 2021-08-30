8289d67995de9cbaeedfa6d1ff1139fa80243b9e
Gabriel <gabriel.gabrielyea@gmail.com>
2021-08-30 18:35:39 -0500

    Add style to CissionContainer

--- src/components/missionContainer/MissionContainer.js
+++ src/components/missionContainer/MissionContainer.js
@@ -2,6 +2,8 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 // import { motion } from 'framer-motion';
 import { fetchAllMissions } from '../../redux/missions/missionsSlice';
+import Mission from '../mission/Mission';
+import styles from './missionContainerStyle.module.scss';
 
 const MissionContainer = () => {
   const dispatch = useDispatch();
@@ -9,21 +11,31 @@ const MissionContainer = () => {
 
   useEffect(() => {
     dispatch(fetchAllMissions());
-  }, []);
+  }, [dispatch]);
 
-  const createMissions = () => allMissions.map((mission) => (
-    <div key={mission.mission_id}>
-      {mission.mission_name}
-    </div>
+  const createMissions = (list) => list.map((mission) => (
+    <Mission
+      key={mission.mission_id}
+      id={mission.mission_id}
+      name={mission.mission_name}
+      description={mission.description}
+      member={mission.reserved}
+    />
   ));
 
   return (
     <>
       {allMissions
       && (
-      <ul>
-        {createMissions()}
-      </ul>
+      <table className={styles.mainContainer}>
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th> </th>
+        </tr>
+        {createMissions(allMissions)}
+      </table>
       )}
     </>
   );
