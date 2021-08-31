e7ac3edec47e0d429ca2fc1b0ee7d2175b8094ca
Gabriel <gabriel.gabrielyea@gmail.com>
2021-08-30 21:02:08 -0500

    Remove comments

--- src/redux/missions/missionsSlice.js
+++ src/redux/missions/missionsSlice.js
@@ -7,22 +7,11 @@ export const fetchAllMissions = createAsyncThunk('missions/fetchAllMissions', as
   const newArr = response.data.map((miss) => ({
     mission_name: miss.mission_name,
     mission_id: miss.mission_id,
-    reserved: 'no',
+    reserved: 'false',
     description: miss.description,
   }));
   return newArr;
 });
-
-// export const createBook = createAsyncThunk('books/createBook', async ({
-//   item_id, author, category, title,
-// }) => {
-//   title = title.concat(`/${author}`);
-//   await access.postApi(routes.MAIN, { item_id, category, title });
-// });
-
-// export const deleteBook = createAsyncThunk('books/deleteBook', async ({ id }) => {
-//   await access.deleteApi(routes.MAIN, { item_id: id });
-// });
 
 export const missionsSlice = createSlice({
   name: 'missions',
@@ -31,10 +20,10 @@ export const missionsSlice = createSlice({
     toggle: (state, action) => {
       const found = (
         state.entities.find((mission) => mission.mission_id === action.payload));
-      if (found.reserved === 'yes') {
-        found.reserved = 'no';
+      if (found.reserved === 'true') {
+        found.reserved = 'false';
       } else {
-        found.reserved = 'yes';
+        found.reserved = 'true';
       }
     },
   },
@@ -47,23 +36,6 @@ export const missionsSlice = createSlice({
         state.entities = [...action.payload];
         state.loading = 'idle';
       });
-    // builder
-    //   .addCase(createBook.fulfilled, (state, action) => {
-    //     state.loading = 'idle';
-    //     state.entities = [...state.entities, action.meta.arg];
-    //   })
-    //   .addCase(createBook.pending, (state) => {
-    //     state.loading = 'pending';
-    //   });
-    // builder
-    //   .addCase(deleteBook.fulfilled, (state, action) => {
-    //     state.loading = 'idle';
-    //     const filterArr = state.entities.filter((book) => book.item_id !== action.meta.arg.id);
-    //     state.entities = filterArr;
-    //   })
-    //   .addCase(deleteBook.pending, (state) => {
-    //     state.loading = 'pending';
-    //   });
   },
 });
 
